b44f273eb1a3dc2abafeaffe99bca26f5155189c
Veronika <EugeneJester@mail.ru>
2023-04-28 15:20:59 +1200

    создана папка 2

---
+++ task_2/2.js
@@ -0,0 +1,15 @@
+"use strict";
+printGreet(prompt("Введите Ваше имя: "));
+
+function printGreet(name) {
+    alert(`Приветствую Вас, ${name}!`);
+}
+2/
+function Grettings() {
+    let name = prompt('Как вас зовут?');
+    msg = `Привет, ${name}!!`
+    alert(msg)
+};
+
+Grettings();
+console.log(msg);
